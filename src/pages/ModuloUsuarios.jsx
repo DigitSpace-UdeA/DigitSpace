14a95cbbe0965af06dbf7fe358f2033409d964ae
Yesica <yesicavillamil@gmail.com>
2021-10-11 17:44:03 -0500

    datos dinamicos json dentro de la app

--- src/pages/ModuloUsuarios.jsx
+++ src/pages/ModuloUsuarios.jsx
@@ -8,92 +8,90 @@ import Pendiente from '../media/Pendiente.svg'
 import autorizado from '../media/autorizado.svg'
 import no_aut from '../media/no_Autorizado.svg'
 
-const ModuloUsuarios = () => {
+const ModuloUsuarios = ({ listaUsuarios }) => {
 
     const usuariosBD = [
         {
             nombre: "Juan",
             rol: "Admin",
-            Estado: "No Auth"
+            estado: "No Auth"
         },
         {
             nombre: "Pepito",
             rol: "Vendedor",
-            Estado: "Auth"
+            estado: "Auth"
         },
         {
             nombre: "Poter",
             rol: "Vendedor",
-            Estado: "Pendiente"
+            estado: "Pendiente"
         }
     ]
 
     // Estado
     const [usuarios, setUsuarios] = useState([])
 
+    /* const [estado, setEstado] = useState("") */
+
     useEffect(() => {
         // traer informacion de bk. Obtener lista de usuarios. variable vacia se ejecuta la primera vez del render
         setUsuarios(usuariosBD)
     }, [])
 
+    listaUsuarios = usuarios;
+
     return (
         <div >
             <h1 className="titlulo">Modulo de Usuarios y Roles</h1>
-            <TablaUsuarios listaUsuarios = {usuarios} />
+            <div className="section-table">
+                <table className="tabla-usuarios">
+                    <thead>
+                        <tr>
+                            {/* <th className="table-element" scope="col">ID</th> */}
+                            <th className="table-element" scope="col">Usuario</th>
+                            <th className="table-element" scope="col">Rol</th>
+                            <th className="table-element" scope="col">Estado</th>
+                            <th className="table-element" scope="col" colspan="2">Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* .map le paso un array en tipo JSON y devuelve un array de htlm */}
+                        {listaUsuarios.map((usuario) => {
+                            return (
+                                <tr>
+                                    {/* <th className="table-element" scope="row">1</th> */}
+                                    <td className="table-element" >{usuario.nombre}</td>
+                                    <td className="table-element" >{usuario.rol}</td>
+                                    <td className="table-element" >
+                                    {usuario.estado}
+                                        {/* <img title="Pendiente" src={Pendiente} alt="imagen" className="icon-eliminar" /> */}
+                                    </td>
+                                    <td className="table-element">
+                                        <a href="./editarUsuario.html">
+                                            <button title="Editar" type="button" className=" button-gen btn-opcion">
+
+                                                <i className="fas fa-edit"></i>
+                                            </button>
+                                        </a>
+                                    </td>
+                                    <td className="table-element">
+                                        <a href="./eliminarUsuario.html">
+                                            <button title="Eliminar" type="button" className="button-gen btn-opcion">
+                                                <i className="fas fa-trash-alt"></i>
+                                            </button>
+                                        </a>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
 
 
     );
 };
-
-const TablaUsuarios = ({ listaUsuarios }) =>{
-    console.log("prueba")
-    return (
-        <div className="section-table">
-            <table className="tabla-usuarios">
-                <thead>
-                    <tr>
-                        {/* <th className="table-element" scope="col">ID</th> */}
-                        <th className="table-element" scope="col">Usuario</th>
-                        <th className="table-element" scope="col">Rol</th>
-                        <th className="table-element" scope="col">Estado</th>
-                        <th className="table-element" scope="col" colspan="2">Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {/* .map le paso un array en tipo JSON y devuelve un array de htlm */}
-                    {listaUsuarios.map((usuario) => {
-                        return (
-                            <tr>
-                                {/* <th className="table-element" scope="row">1</th> */}
-                                 <td className="table-element" >{usuario.nombre}</td>
-                                <td className="table-element" >{usuario.rol}</td> 
-                                <td className="table-element" >
-                                    <img title="Pendiente" src={Pendiente} alt="imagen" className="icon-eliminar" />
-                                </td>
-                                <td className="table-element">
-                                    <a href="./editarUsuario.html">
-                                        <button title="Editar" type="button" className=" button-gen btn-opcion">
-
-                                            <i className="fas fa-edit"></i>
-                                        </button>
-                                    </a>
-                                </td>
-                                <td className="table-element">
-                                    <a href="./eliminarUsuario.html">
-                                        <button title="Eliminar" type="button" className="button-gen btn-opcion">
-                                            <i className="fas fa-trash-alt"></i>
-                                        </button>
-                                    </a>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
-    );
-}
 
 export default ModuloUsuarios;
 
