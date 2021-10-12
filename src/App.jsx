927ec7a6c63abecafa50bfd72f174807a76e9ed2
David <davico.gonzalez@live.com>
2021-10-12 13:10:27 -0500

    Ajuste tailwind al proyecto.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import './App.css';
-
-
-
-import logo from './media/logo.svg';
 import Layout from "./layouts/layout";
-
-
-
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Main from './pages/Main'
 import RegistroVentas from './pages/registroVentas';
 import EditarUsuario from './pages/editarUsuario';
@@ -20,22 +11,56 @@ import ProductosRegistro from './pages/productosRegistro';
 import ModuloUsuarios from './pages/ModuloUsuarios';
 import LandingPage from './layouts/landingPage';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className='App'>
-      <div className='bg-red-400'> Hola mundo</div>
-
+    <div className="App">
+      <Router>
+        {/* <Link to="/">Home</Link> <Link to="/ModuloUsuarios">Usuarios</Link> */}
+        <Switch>
+          <Route path={["/ModuloUsuarios", "/editarUsuario", "/eliminarUsuario", "/productosMaestro", "/productosRegistro", "/registroVentas", "/ventasMaestro", "/main"]} exact>
+            <Layout>
+              <Switch>
+                <Route path="/ModuloUsuarios" exact>
+                  <ModuloUsuarios />
+                </Route>
+                <Route path="/editarUsuario" exact>
+                  <EditarUsuario />
+                </Route>
+                <Route path="/eliminarUsuario" exact>
+                  <EliminarUsuario />
+                </Route>
+                <Route path="/productosMaestro" exact>
+                  <ProductosMaestro />
+                </Route>
+                <Route path="/productosRegistro" exact>
+                  <ProductosRegistro />
+                </Route>
+                <Route path="/registroVentas" exact>
+                  <RegistroVentas />
+                </Route>
+                <Route path="/ventasMaestro" exact>
+                  <VentasMaestro />
+                </Route>
+                <Route path="/main" exact>
+                  <Main />
+                </Route>
+              </Switch>
+            </Layout>
+          </Route>
+          <Route path={["/"]}>
+            <LandingPage>
+              <Switch>
+                <Route path="/" exact>
+                  <LoginModule />
+                </Route>
+              </Switch>
+            </LandingPage>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
