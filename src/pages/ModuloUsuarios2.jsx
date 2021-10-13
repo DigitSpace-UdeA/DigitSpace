f7a3b2e4704fe3b52acc25393b5f34989f3b9c00
katerinBarrera <katerinbarrera2001@gmail.com>
2021-10-13 08:29:52 -0500

    modulousuarios done

--- src/pages/ModuloUsuarios2.jsx
+++ src/pages/ModuloUsuarios2.jsx
@@ -1,69 +1,83 @@
 import '../style/stylesUser.css';
+import React, { useEffect, useState } from 'react'
 import Pendiente from '../media/Pendiente.svg'
 import autorizado from '../media/autorizado.svg'
 import no_aut from '../media/no_Autorizado.svg'
 
-const ModuloUsuarios = () => {
-  //return (
 
-    const TablaUsuarios = () =>{
+
+const ModuloUsuarios = () => {
+
+    const ModuloUsuariosBackend = [
+        {
+            ID: 1,
+            Usuario: "Mark",
+            Rol: "Vendedor",
+            Estado: "Autorizado",
+        },
+        {
+            ID: 2,
+            Usuario: "Mark",
+            Rol: "Vendedor",
+            Estado: "Autorizado",
+        },
+        {
+            ID: 3,
+            Usuario: "Mark",
+            Rol: "Vendedor",
+            Estado: "Autorizado",
+        }
+    ];
+
+    const [listaModuloUsuarios, setlistaModuloUsuarios]= useState([]);
+    useEffect(() => {
+        //obtener lista desde el front
+        setlistaModuloUsuarios(ModuloUsuariosBackend)
+    }, []);
+    
+    return (
     <div className= 'flex flex-col items-center justify-center'>
-        <h2 className = 'text-2xl font-extrabold text-gray-800'> Modulo de usuarios y roles</h2>
+        <h2 className = 'text-2xl font-extrabold text-gray-800'> Modulo de ModuloUsuarios y roles</h2>
         <table>
-            <thead>
-                <tr>
-                <th>ID</th>
-                <th>Usuario</th>
-                <th>Rol</th>
-                <th>Estado</th>
-                <th>Opciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Vendedor</td>
-                    <td>Autorizado</td>
-                    <tr><div className = 'flex'>
-                    <td><a href="editarUsuario">
+        <thead>
+            <tr>
+            <th>ID</th>
+            <th>Usuario</th>
+            <th>Rol</th>
+            <th>Estado</th>
+            <th>Opciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {listaModuloUsuarios.map((usuario) => {
+                return (
+                    <tr>
+                    <td>{usuario.ID}</td>
+                    <td>{usuario.Usuario}</td>
+                    <td>{usuario.Rol}</td>
+                    <td>{usuario.Estado}</td>
+                    <td>
+                    <div className = 'flex w-full justify-around'>
+                        <a href="editarUsuario">
                         <button title="Editar" type="button" className=" button-gen btn-opcion">
                         <i className="fas fa-edit"></i>
-                        </button></a></td> 
-                    <td><a href="eliminarUsuario">
+                        </button></a>
+                        <a href="eliminarUsuario">
                         <button title="Eliminar" type="button" className="button-gen btn-opcion">
                         <i className="fas fa-trash-alt"></i>
-                        </button>
-                    </a></td></div></tr>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Vendedor</td>
-                    <td>Autorizado</td>
-                    <tr><div className = 'flex'>
-                    <td><a href="editarUsuario">
-                        <button title="Editar" type="button" className=" button-gen btn-opcion">
-                        <i className="fas fa-edit"></i>
-                        </button></a></td> 
-                    <td><a href="eliminarUsuario">
-                        <button title="Eliminar" type="button" className="button-gen btn-opcion">
-                        <i className="fas fa-trash-alt"></i>
-                        </button>
-                    </a></td></div></tr>
-                </tr>
-            </tbody>
+                        </button></a>
+                    </div>
+                    </td> 
+                    </tr>
+                )
+            })}
+        </tbody>
         </table>
     </div>
-    };
+          
+)};
 
 
 
-    
-
-
-
-
-};
 
 export default ModuloUsuarios;
